abffa8ccd7d4fe57387bb112f5e4161bbb4c5104
Jacob Carpenter <github@jacobcarpenter.com>
2020-05-03 13:35:42 -0700

    Add page title and theme color and remove gatsby favicon.

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,9 @@
  */
 
 module.exports = {
-	plugins: ['gatsby-plugin-styled-components', 'gatsby-plugin-react-svg'],
+	plugins: [
+		'gatsby-plugin-styled-components',
+		'gatsby-plugin-react-svg',
+		'gatsby-plugin-react-helmet',
+	],
 };
--- src/components/layout/PageLayout.jsx
+++ src/components/layout/PageLayout.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import { Helmet } from 'react-helmet';
 import { GlobalStyle } from './GlobalStyle';
 import { PageHeader } from './PageHeader';
 import { Box, theme } from '../styled';
@@ -7,6 +8,10 @@ import { Box, theme } from '../styled';
 export function PageLayout({ children, width = 600 }) {
 	return (
 		<ThemeProvider theme={theme}>
+			<Helmet>
+				<meta name="theme-color" content={theme.colors.primary} />
+				<title>jacobcarpenter.com</title>
+			</Helmet>
 			<GlobalStyle />
 			<PageHeader maxWidth={600} />
 			<Box mt={[3]} mx={[3]} display="flex" justifyContent="center">
